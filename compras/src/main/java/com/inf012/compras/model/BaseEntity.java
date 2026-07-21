c300e4ea1b0a75b55c1a21d023b1ee8669dbe009
Luan Vitor Ribeiro Guimarães <luanvitor.mat@gmail.com>
2026-07-21 16:24:27 -0300

    Merge com docker-compose

--- compras/src/main/java/com/inf012/compras/model/BaseEntity.java
+++ compras/src/main/java/com/inf012/compras/model/BaseEntity.java
@@ -9,7 +9,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 
+@MappedSuperclass
 public abstract class BaseEntity {
 
     @Id
